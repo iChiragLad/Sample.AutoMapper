839f56d914511719b1e2e28eafaf1fef3ffe6edb
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-05-11 16:42:02 +0530

    Mapper Configuration

---
+++ AutoMapper.Practice/AutoMapper.Practice.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AutoMapper.Practice.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cnfg =>
+            {
+                cnfg.CreateMap<Person, Employee>().BeforeMap((p, e) => p.Age = 25).AfterMap((p, e) => { e.Experience = (p.Age - 24); });
+            });
+            var mapper = config.CreateMapper();
+
+            Person person = new Person();
+            person.Age = 30;
+            person.FirstName = "Chirag";
+            person.LastName = "Lad";
+
+            Employee emp = mapper.Map<Employee>(person);
+
+
+            System.Console.ReadKey();
+        }
+    }
+}
