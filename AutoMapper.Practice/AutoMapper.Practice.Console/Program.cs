d8f4f2761cf2406bf6e87610c9a8acfea94598f2
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-05-12 09:28:17 +0530

    Complete AutoMapper usage

--- AutoMapper.Practice/AutoMapper.Practice.Console/Program.cs
+++ AutoMapper.Practice/AutoMapper.Practice.Console/Program.cs
@@ -7,25 +7,102 @@ using AutoMapper;
 
 namespace AutoMapper.Practice.Console
 {
-    class Program
+    class MappingProfile : Profile
     {
-        static void Main(string[] args)
+        protected override void Configure()
         {
-            var config = new MapperConfiguration(cnfg =>
+            //creating a map from Person to Employee
+            CreateMap<Person, Employee>()
+                //Before map action
+                .BeforeMap((p, e) => p.Age = 25)
+                //After map action
+                .AfterMap((p, e) => { e.Experience = (p.Age - 24); })
+                //projection
+                .ForMember(e => e.Gender, cexp => cexp.MapFrom(e => e.Sex))
+                //Conditional Mapping
+                .ForMember(x => x.FirstName, c => c.Condition(p => (p.FirstName.Length > 1)))
+                //projection
+                .ForMember(x => x.Experience, c => c.MapFrom(p => p.Age))
+                //ignoring map
+                .ForMember(x => x.Id, c => c.Ignore())
+                //constructor parameter mapping
+                .ForCtorParam("value", c => c.MapFrom(p => p.Address))
+                //custom type resolver
+                .ForMember(x => x.Salary, c => c.ResolveUsing(new SalaryResolver()))
+                //projection
+                .ForMember(x => x.Manager, c => c.MapFrom(p => p.Parent))
+                //Null substitution
+                .ForMember(x => x.Manager, c => c.NullSubstitute("Fill Later"));
+            //custom value resolver
+            CreateMap<bool, string>().ConvertUsing(new MaritalStatusConverter());
+        }
+
+        class ConfigurationProfile : Profile
+        {
+            protected override void Configure()
             {
-                cnfg.CreateMap<Person, Employee>().BeforeMap((p, e) => p.Age = 25).AfterMap((p, e) => { e.Experience = (p.Age - 24); }).ForMember(e => e.Gender, cexp => cexp.MapFrom(e => e.Sex));
-            });
-            var mapper = config.CreateMapper();
+                ShouldMapProperty = (pi => pi.GetMethod.IsPublic);
+                //RecognizeDestinationPrefixes("emp");
+                //ReplaceMemberName("Addresi", "Address");
+            }
+        }
 
-            Person person = new Person();
-            person.Age = 30;
-            person.FirstName = "Chirag";
-            person.LastName = "Lad";
+        class MaritalStatusConverter : ITypeConverter<bool, string>
+        {
+            public string Convert(ResolutionContext context)
+            {
+                if ((bool)context.SourceValue)
+                {
+                    return "Single";
+                }
+                else
+                {
+                    return "Married";
+                }
+            }
+        }
 
-            Employee emp = mapper.Map<Employee>(person);
+        class SalaryResolver : ValueResolver<Person, int>
+        {
+            protected override int ResolveCore(Person source)
+            {
+                if(source.Educated)
+                {
+                    return 300000;
+                }
+                else
+                {
+                    return 100000;
+                }
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var config = new MapperConfiguration(cnfg =>
+                {
+                    cnfg.AddProfile(new MappingProfile());
+                    cnfg.AddProfile(new ConfigurationProfile());
+                });
 
+                config.AssertConfigurationIsValid();
+                var mapper = config.CreateMapper();
 
-            System.Console.ReadKey();
+                Person person = new Person();
+                person.Age = 30;
+                person.FirstName = "Chirag";
+                person.LastName = "Lad";
+                person.Sex = "Male";
+                person.Address = "Bhagwati Ashiyana";
+                person.Marital = true;
+                person.Educated = true;
+                person.Parent = null;
+
+                Employee emp = mapper.Map<Employee>(person);
+
+                System.Console.ReadKey();
+            }
         }
     }
 }
--- AutoMapper.Practice/AutoMapper.Practice.Console/SampleClasses.cs
+++ AutoMapper.Practice/AutoMapper.Practice.Console/SampleClasses.cs
@@ -8,10 +8,26 @@ namespace AutoMapper.Practice.Console
 {
     class Employee
     {
+        private string _address;
+        public Employee(string value)
+        {
+            _address = value;
+        }
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+        }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Experience { get; set; }
         public string Gender { get; set; }
+        public string Id { get; set; }
+        public string Marital { get; set; }
+        public int Salary { get; set; }
+        public string Manager { get; set; }
     }
     class Person
     {
@@ -19,5 +35,10 @@ namespace AutoMapper.Practice.Console
         public string LastName { get; set; }
         public string Sex { get; set; }
         public int Age { get; set; }
+        public string Address { get; set; }
+        public bool Marital { get; set; }
+        public bool Educated { get; set; }
+        public string Parent { get; set; }
+
     }
 }
